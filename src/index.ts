96d8ee3cc749d182caf5e31ddd32a5ae9d90e9c9
崮生 <admin@shenzilong.cn>
2024-05-16 11:16:30 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,42 @@
 import { Plugin } from "siyuan";
+import { sql } from "./api";
+import { hash_time33 } from "@llej/js_util/src/hash";
 
-export default class OceanPress extends Plugin {
-  private btn_selector = "oceanpress_widget_button";
-  private timeID: ReturnType<typeof setInterval> | null = null;
+type attribute = {
+  block_id: "id";
+  box: "id";
+  id: "id";
+  name: "bookmark";
+  path: "/id/id.sy";
+  root_id: "id";
+  type: "b";
+  value: "expr";
+};
 
-  async onLayoutReady() {
-    console.log("启动");
-
+export default class expr extends Plugin {
+  async onload() {
+    /** 查询所有 bookmark = 'expr' or name = "custom-expr" 的属性 */
+    const attr: attribute[] = await sql(`
+    SELECT * FROM attributes
+    WHERE
+      name = "custom-expr"
+      OR
+      (name = "bookmark" AND VALUE ="expr")`);
+    console.log("启动", hash_time33("dfadfa"), attr, filterNotInitBlock(attr));
   }
-  async onunload() {
-  }
-
-  async addButton() {
-  }
-  previewCurrentPage() {}
+  async onunload() {}
 }
-function sleep(time: number) {
-  return new Promise((s) => setTimeout(s, time));
+function filterNotInitBlock(attrs: attribute[]) {
+  const idCount: { [block_id: string]: { count: number; attrs: attribute[] } } = {};
+  attrs.forEach(async (attr: attribute) => {
+    if (idCount[attr.block_id] === undefined) {
+      idCount[attr.block_id] = { count: 0, attrs: [] };
+    }
+    idCount[attr.block_id].count += 1;
+    idCount[attr.block_id].attrs.push(attr);
+  });
+  return Object.keys(idCount)
+    .filter((block_id: string) => idCount[block_id].count === 1)
+    .map((block_id: string) => idCount[block_id].attrs[0]);
 }
+async function exprBlockInit(block: any) {}
