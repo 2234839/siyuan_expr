ef2fd5565b6842490aaabd3e0a41656dcf48dcc3
崮生 <admin@shenzilong.cn>
2024-06-05 13:09:06 +0800

    fix plugin.json

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,23 @@ type aliasAttribute = { [K in keyof attribute as `a_${K}`]: attribute[K] };
 type MergedBlock = aliasAttribute & Block;
 
 const dev = console.log;
-
+declare global {
+  var Expr: Expr;
+}
 export default class Expr extends Plugin {
   IntervalId = 0;
   /** 只更新这个时间戳以后的表达式 */
-  static updated = 0;
+  updated = 0;
   /** 为 true 代表正在进行求值运算中 */
-  static evalState = false;
+  evalState = false;
   async onload() {
+    globalThis.Expr = this;
+    this.onunloadFn.push(() => {
+      //@ts-ignore
+      delete globalThis.Expr;
+    });
+
+    const updateExprEval = this.updateExprEval.bind(this);
     this.IntervalId = setInterval(updateExprEval, 1_000);
     updateExprEval();
     this.onunloadFn.push(() => clearInterval(this.IntervalId));
@@ -33,72 +42,68 @@ export default class Expr extends Plugin {
     dev("expr plugin unload");
     this.onunloadFn.forEach((fn) => fn());
   }
-}
-/** 对最近更新过的表达式进行求值 */
-async function updateExprEval() {
-  if (Expr.evalState) {
-    /** 只有上一轮求值计算进行完毕后才会开始新一轮计算 */
-    return;
-  }
-  try {
-    Expr.evalState = true;
-    const exprAttr: MergedBlock[] = await sql(
-      `SELECT b.*,a.id AS a_id,a."name" AS a_name,a."value" as a_value,a."type" AS a_type,a.block_id AS a_block_id,a.root_id AS a_root_id,a.box AS a_box,a."path" AS a_path
+
+  /** 对最近更新过的表达式进行求值 */
+  async updateExprEval() {
+    if (this.evalState) {
+      /** 只有上一轮求值计算进行完毕后才会开始新一轮计算 */
+      return;
+    }
+    try {
+      this.evalState = true;
+      const exprAttr: MergedBlock[] = await sql(
+        `SELECT b.*,a.id AS a_id,a."name" AS a_name,a."value" as a_value,a."type" AS a_type,a.block_id AS a_block_id,a.root_id AS a_root_id,a.box AS a_box,a."path" AS a_path
       FROM blocks AS  b
       INNER JOIN attributes AS a
       ON b.id = a.block_id
-      WHERE a.name = "${exprName}" and CAST(b.updated AS INTEGER)  > ${Expr.updated}
+      WHERE a.name = "${exprName}" and CAST(b.updated AS INTEGER)  > ${this.updated}
       ORDER BY b.updated DESC;`,
-    );
-    if (exprAttr.length > 0) {
-      console.log(exprAttr, Expr.updated);
-      await Promise.all(exprAttr.map(exprEval));
+      );
+      if (exprAttr.length > 0) {
+        console.log(exprAttr, this.updated);
+        await Promise.all(exprAttr.map(this.exprEval.bind(this)));
+      }
+    } catch (error) {
+      dev("求值错误", error);
+    } finally {
+      this.evalState = false;
     }
-  } catch (error) {
-    dev("求值错误", error);
-  } finally {
-    Expr.evalState = false;
+  }
+
+  async exprEval(block: MergedBlock) {
+    const evalValue = await eval(block.a_value);
+
+    const updated = generateTimestamp();
+    if (Number(updated) > this.updated) {
+      this.updated = Number(updated);
+    }
+    /** TODO,这里应该要考虑ial中不存在相关字段的情况，需要进行添加而非替换 更新块的update时间戳
+     * ial = `{: updated="20240604233920" custom-expr="10-11+Math.random()+&quot;2&quot;" custom-expr-value="-0.95897021536132312" id="20240514180539-3zvaoab" style="background-color: var(--b3-font-background4);"} `
+     */
+    let newKramdownAttr = block.ial!;
+    if (/updated="\d+"/.test(newKramdownAttr)) {
+      newKramdownAttr = newKramdownAttr.replace(/updated="\d+"/, `updated="${updated}"`);
+    } else {
+      newKramdownAttr = newKramdownAttr.replace(/}$/, ` updated="${updated}"`);
+    }
+    if (/custom-expr-value=".*?"/.test(newKramdownAttr)) {
+      newKramdownAttr = newKramdownAttr.replace(
+        /custom-expr-value=".*?"/,
+        `custom-expr-value="${encodeHTML(String(evalValue))}"`,
+      );
+    } else {
+      newKramdownAttr = newKramdownAttr.replace(
+        /}$/,
+        ` custom-expr-value="${encodeHTML(String(evalValue))}"`,
+      );
+    }
+    // custom-expr-value="-0.56273369360008952"
+    /** 将求值结果更新到块文本 */
+    await updateBlock("markdown", String(evalValue + "\n" + newKramdownAttr), block.id);
+    dev(newKramdownAttr, this.updated);
   }
 }
 
-async function exprEval(block: MergedBlock) {
-  const evalValue = await eval(block.a_value);
-
-  const updated = generateTimestamp();
-  if (Number(updated) > Expr.updated) {
-    Expr.updated = Number(updated);
-  }
-  /** TODO,这里应该要考虑ial中不存在相关字段的情况，需要进行添加而非替换 更新块的update时间戳
-   * ial = `{: updated="20240604233920" custom-expr="10-11+Math.random()+&quot;2&quot;" custom-expr-value="-0.95897021536132312" id="20240514180539-3zvaoab" style="background-color: var(--b3-font-background4);"} `
-   */
-  let newKramdownAttr = block.ial!;
-  if (/updated="\d+"/.test(newKramdownAttr)) {
-    newKramdownAttr = newKramdownAttr.replace(/updated="\d+"/, `updated="${updated}"`);
-  } else {
-    newKramdownAttr = newKramdownAttr.replace(/}$/, ` updated="${updated}"`);
-  }
-  if (/custom-expr-value=".*?"/.test(newKramdownAttr)) {
-    newKramdownAttr = newKramdownAttr.replace(
-      /custom-expr-value=".*?"/,
-      `custom-expr-value="${encodeHTML(String(evalValue))}"`,
-    );
-  } else {
-    newKramdownAttr = newKramdownAttr.replace(
-      /}$/,
-      ` custom-expr-value="${encodeHTML(String(evalValue))}"`,
-    );
-  }
-  // custom-expr-value="-0.56273369360008952"
-  /** 将求值结果更新到块文本 */
-  await updateBlock("markdown", String(evalValue + "\n" + newKramdownAttr), block.id);
-  dev(newKramdownAttr, Expr.updated);
-}
-
-async function getBlockByID(blockId: string): Promise<Block> {
-  let sqlScript = `select * from blocks where id ='${blockId}'`;
-  let data = await sql(sqlScript);
-  return data[0];
-}
 function generateTimestamp() {
   const now = new Date();
   return [
